ecb146dba8e901aa14f2d76b02a8f04ce9f99ac3
Uta Toyda <106716031+yutatoida@users.noreply.github.com>
2024-06-19 00:36:46 +0900

    add js

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 # 感情分析パイプラインの初期化
 sentiment_pipeline = pipeline("sentiment-analysis")
 
+# ラベルを日本語にマッピングする辞書
+label_mapping = {
+    'LABEL_0': 'ネガティブ',
+    'LABEL_1': 'ポジティブ',
+    'POSITIVE': 'ポジティブ',
+    'NEGATIVE': 'ネガティブ'
+}
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,8 +22,8 @@ def home():
 def analyze():
     text = request.form['text']
     result = sentiment_pipeline(text)[0]
+    result['label'] = label_mapping.get(result['label'], '未知のラベル')
     return render_template('index.html', text=text, result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
